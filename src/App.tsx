714013147e4c9663bd83f6be831f0a9bb610afe5
EdwinDev99 <edwindev99@gmail.com>
2025-05-28 13:17:58 +0100

    fix loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { useEffect, useState } from "react";
-import { onAuthStateChanged, type User } from "firebase/auth"; // importa el tipo 'User'
-import { auth } from "../src/assets/firebase/firebase";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "./assets/firebase/firebase";
 import Restaurant from "./assets/Components/Restaurant";
 import Login from "./assets/Components/pages/Login";
 
 function App() {
-  const [user, setUser] = useState<User | null>(null); // especifica el tipo correctamente
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       setUser(firebaseUser);
+      setLoading(false);
     });
+
     return () => unsubscribe();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <div className="spinner-border text-primary" role="status" />
+      </div>
+    );
+  }
 
   return <div className="container">{user ? <Restaurant /> : <Login />}</div>;
 }
